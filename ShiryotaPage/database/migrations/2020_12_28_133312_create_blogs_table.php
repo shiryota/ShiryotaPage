17c249396c653949e2caa27c3159a551eec395d3
shiryota <shiryo19950420@gmail.com>
2021-01-03 23:29:43 +0900

    コメント、コンポーネント整理

---
+++ ShiryotaPage/database/migrations/2020_12_28_133312_create_blogs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('url');
+            $table->string('abstract');
+            $table->string('title');
+            $table->integer('date');
+            $table->string('tag1');
+            $table->string('body');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blogs');
+    }
+}
---
+++ ShiryotaPage/resources/views/components/head.blade.php
@@ -0,0 +1,11 @@
+<head>
+    <meta charset = "utf-8">
+    <title>{{$Title}}</title>
+    <meta name = "description" content="ITで稼ぎたいシリョタのページ">
+
+    <!-- CSS -->
+        <link href="{{asset('css/ress.css')}}" rel="stylesheet">
+        <link href="{{asset('css/style.css')}}" rel="stylesheet">
+    <!-- Favicon -->
+        <link rel = "icon" type = "image/png" href = "images/favi.png">
+</head>
